838832dd49632f6a84d9fe888017063b7c92f051
Borisenko Natalli <nata251284@gmail.com>
2023-03-12 18:31:43 +0300

    Add SecondArrayWithIF

--- Task_final/Program.cs
+++ Task_final/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] array1 = new string[7] {"123", "23", "hello", "world", "res", "Russia", ":-)"};
+string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
